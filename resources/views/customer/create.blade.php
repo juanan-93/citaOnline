c810d19221e8ed75ee2299f3ca0cdb84e55c1d40
juanan-93 <juanan-93@hotmail.com>
2024-08-16 23:47:16 +0200

    create

---
+++ resources/views/customer/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Gestión de Clientes')
+@section('breadcrumb')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="#">Clientes</a></li>
+        <li class="breadcrumb-item active">Crear  Clientes</li>
+    </ol>
+@endsection
+
+@section('content')
+   
+@endsection
+
+
